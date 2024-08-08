ed0a2d45dbc719400a9683dc20b7576d1d181589
adriansito124 <adriangobara@gmail.com>
2024-08-08 20:31:41 -0300

    Fixed everything :ok_hand:

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+
+const modal = document.querySelector(".modal");
+const menu = document.querySelector(".menu");
+
+let show = false;
+
+menu.addEventListener("click", () => {
+    show = !show;
+    modal.style.display = show ? 'none' : 'flex';
+})
+
 document.addEventListener("DOMContentLoaded", () => {
     const cd = document.querySelector(".cd");
     const screen = document.querySelector(".screen");
     const cdWidth = cd.clientWidth;
     const cdHeight = cd.clientHeight;
-    
     let x = 0;
     let y = 0;
     let vx = 1.5; //velocidade
-    let vy = 1.5; 
+    let vy = 1.5;
     let trade = 0;
+
+    
 
     function update() {
         const screenWidth = screen.clientWidth;
         const screenHeight = screen.clientHeight;
-        
+
         x += vx;
         y += vy;
 
