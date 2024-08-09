9c882c55067a28f602cdbfe6ce2031799763e89a
Adrian Gobara <adriangobara@gmail.com>
2024-08-09 00:10:13 -0300

    troca imagem :)

--- script.js
+++ script.js
@@ -29,12 +29,52 @@ document.addEventListener("DOMContentLoaded", () => {
         x += vx;
         y += vy;
 
+        var image = document.getElementById("disco");
+
+        if (trade == 0) {
+            image.src = "./Imagens/Ibere.jpeg";
+        }
+
+        if (trade == 1) {
+            image.src = "./Imagens/adrian.jpeg";
+        }
+
+        if (trade == 2) {
+            image.src = "./Imagens/festa.jpeg";
+        }
+
+        if (trade == 3) {
+            image.src = "./Imagens/militar.jpeg";
+        }
+
+        if (trade == 4) {
+            image.src = "./Imagens/natal.jpeg";
+        }
+
+        if (trade == 5) {
+            image.src = "./Imagens/snorlax.jpeg";
+        }
+
+        if (trade == 6) {
+            image.src = "./Imagens/terno.jpeg";
+        }
+
+        if (trade == 7) {
+            image.src = "./Imagens/matias.png";
+        }
+
         if (x + cdWidth > screenWidth || x < 0) {
             vx *= -1;
+            trade++;
         }
 
         if (y + cdHeight > screenHeight || y < 0) {
             vy *= -1;
+            trade++;
+        }
+
+        if (trade > 7) {
+            trade = 0;
         }
 
         cd.style.transform = `translate(${x}px, ${y}px)`;
